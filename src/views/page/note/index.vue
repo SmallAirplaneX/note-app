e6443592aa6d7ad51e02fb8c0a927002a406134d
SmallAirplaneX <2321764325@qq.com>
2024-03-17 22:32:21 +0800

    删除无关代码

--- src/views/page/note/index.vue
+++ src/views/page/note/index.vue
@@ -1,27 +1,10 @@
 <script setup>
-import Editor from '@tinymce/tinymce-vue'
 </script>
 
 <template>
-  <main id="sample">
-    <Editor
-        api-key="27kukh9yjg2qgrl3778ypghh3pw8n1evzscnu818sxwvqknv"
-        :init="{
-        plugins: 'lists link image table code help wordcount'
-      }"
-    />
-  </main>
 
 
 </template>
 
 <style scoped>
-@media (min-width: 1024px) {
-  #sample {
-    display: flex;
-    flex-direction: column;
-    place-items: center;
-    width: 1000px;
-  }
-}
 </style>
